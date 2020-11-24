4f77a0965ae6a3f2e9a3d066fcb7c414b91ab8a4
dizzle-k <26167322+dizzle-k@users.noreply.github.com>
2020-11-24 12:18:17 +0100

    prepared iterators

--- 2-identity-resolution/IdentityResolutionProject/src/main/java/de/uni_mannheim/informatik/web_data_integration/matching_rules/IR_using_machine_learning_wikidata_sales_daniel.java
+++ 2-identity-resolution/IdentityResolutionProject/src/main/java/de/uni_mannheim/informatik/web_data_integration/matching_rules/IR_using_machine_learning_wikidata_sales_daniel.java
@@ -1,6 +1,8 @@
 package de.uni_mannheim.informatik.web_data_integration.matching_rules;
 
 import java.io.File;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 import de.uni_mannheim.informatik.web_data_integration.comparator.*;
 import de.uni_mannheim.informatik.web_data_integration.comparator.custom_similarity_measure.JaroSimilarity;
@@ -10,7 +12,9 @@ import org.slf4j.Logger;
 import de.uni_mannheim.informatik.dws.winter.matching.MatchingEngine;
 import de.uni_mannheim.informatik.dws.winter.matching.MatchingEvaluator;
 import de.uni_mannheim.informatik.dws.winter.matching.algorithms.RuleLearner;
+import de.uni_mannheim.informatik.dws.winter.matching.blockers.SortedNeighbourhoodBlocker;
 import de.uni_mannheim.informatik.dws.winter.matching.blockers.StandardRecordBlocker;
+import de.uni_mannheim.informatik.dws.winter.matching.rules.Comparator;
 import de.uni_mannheim.informatik.dws.winter.matching.rules.WekaMatchingRule;
 import de.uni_mannheim.informatik.dws.winter.model.Correspondence;
 import de.uni_mannheim.informatik.dws.winter.model.HashedDataSet;
@@ -20,11 +24,13 @@ import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
 import de.uni_mannheim.informatik.dws.winter.model.io.CSVCorrespondenceFormatter;
 import de.uni_mannheim.informatik.dws.winter.processing.Processable;
 import de.uni_mannheim.informatik.dws.winter.similarity.EqualsSimilarity;
+import de.uni_mannheim.informatik.dws.winter.similarity.SimilarityMeasure;
 import de.uni_mannheim.informatik.dws.winter.similarity.string.JaccardOnNGramsSimilarity;
 import de.uni_mannheim.informatik.dws.winter.similarity.string.LevenshteinSimilarity;
 import de.uni_mannheim.informatik.dws.winter.similarity.string.MaximumOfTokenContainment;
 import de.uni_mannheim.informatik.dws.winter.similarity.string.TokenizingJaccardSimilarity;
 import de.uni_mannheim.informatik.dws.winter.utils.WinterLogManager;
+import de.uni_mannheim.informatik.web_data_integration.blocking.VideoGameBlockingKeyByPlatformGenerator;
 import de.uni_mannheim.informatik.web_data_integration.blocking.VideoGameBlockingKeyByTitleGenerator;
 import de.uni_mannheim.informatik.web_data_integration.model.VideoGame;
 import de.uni_mannheim.informatik.web_data_integration.model.VideoGameXMLReader;
@@ -34,6 +40,30 @@ public class IR_using_machine_learning_wikidata_sales_daniel {
 	private static final Logger logger = WinterLogManager.activateLogger("trace");
 
 	public static void main(String[] args) throws Exception {
+
+		ArrayList<SimilarityMeasure<String>> simMeasuresString = new ArrayList<>();
+		simMeasuresString.add(new LevenshteinSimilarity());
+		simMeasuresString.add(new TokenizingJaccardSimilarity());
+		simMeasuresString.add(new JaccardOnNGramsSimilarity(2));
+
+		ArrayList<Comparator<VideoGame, Attribute>> pubDateComparators = new ArrayList<>();
+		pubDateComparators.add(new PubDateComparator(1));
+
+		for (int i = 0; i < simMeasuresString.size(); i++) {
+			for (int j = 0; j < simMeasuresString.size(); j++) {
+				for (int k = 0; k < simMeasuresString.size(); k++) {
+					for (int l = 0; l < pubDateComparators.size(); l++) {
+						System.out.println(String.format("Title - %d: %s", i + 1, simMeasuresString.get(i)));
+						System.out.println(String.format("Platform - %d: %s", j + 1, simMeasuresString.get(j)));
+						System.out.println(String.format("Publisher - %d: %s", k + 1, simMeasuresString.get(k)));
+						System.out.println(String.format("PubDate - %d: %s", l + 1, pubDateComparators.get(l)));
+						System.out.println("");
+					}
+				}
+
+			}
+		}
+		System.exit(0);
 
 		// loading data
 		System.out.println("*\n*\tLoading datasets\n*");
@@ -46,7 +76,8 @@ public class IR_using_machine_learning_wikidata_sales_daniel {
 
 		// load the training set
 		MatchingGoldStandard gsTraining = new MatchingGoldStandard();
-		gsTraining.loadFromCSVFile(new File("data/goldstandard/gold-standard_wikidata_sales.csv"));
+		gsTraining.loadFromCSVFile(
+				new File("data/goldstandard/wikidata_sales/gold-standard_wikidata_sales_training_66_33.csv"));
 
 		// create a matching rule
 		String options[] = new String[] { "-S" };
@@ -55,23 +86,10 @@ public class IR_using_machine_learning_wikidata_sales_daniel {
 		matchingRule.activateDebugReport("data/output/debugResultsMatchingRule.csv", 1000, gsTraining);
 
 		// add comparators
-		matchingRule.addComparator(new TitleComparator(new EqualsSimilarity<String>()));
-		matchingRule.addComparator(new TitleComparator(new LevenshteinSimilarity()));
-		matchingRule.addComparator(new TitleComparator(new TokenizingJaccardSimilarity()));
 		matchingRule.addComparator(new TitleComparator(new JaccardOnNGramsSimilarity(3)));
-		matchingRule.addComparator(new TitleComparator(new MaximumOfTokenContainment()));
-		matchingRule.addComparator(new TitleComparator(new JaroWinklerSimilarity()));
-		matchingRule.addComparator(new TitleComparator(new JaroSimilarity()));
-		matchingRule.addComparator(new PlatformComparator(new TokenizingJaccardSimilarity()));
 		matchingRule.addComparator(new PlatformComparator(new LevenshteinSimilarity()));
-		matchingRule.addComparator(new TitleComparator(new LevenshteinSimilarity()));
-		matchingRule.addComparator(new TitleComparator(new TokenizingJaccardSimilarity()));
-		matchingRule.addComparator(new PublisherComparator(new TokenizingJaccardSimilarity()));
-		matchingRule.addComparator(new PublisherComparator(new LevenshteinSimilarity()));
-		matchingRule.addComparator(new PublisherComparator(new EqualsSimilarity<>()));
-		matchingRule.addComparator(new DeveloperComparator(new TokenizingJaccardSimilarity()));
-		matchingRule.addComparator(new DeveloperComparator(new LevenshteinSimilarity()));
-		matchingRule.addComparator(new DeveloperComparator(new EqualsSimilarity<>()));
+		matchingRule.addComparator(new PublisherComparator(new JaroSimilarity()));
+		matchingRule.addComparator(new PubDateComparator(7));
 
 		// train the matching rule's model
 		System.out.println("*\n*\tLearning matching rule\n*");
@@ -80,13 +98,13 @@ public class IR_using_machine_learning_wikidata_sales_daniel {
 		System.out.println(String.format("Matching rule is:\n%s", matchingRule.getModelDescription()));
 
 		// create a blocker (blocking strategy)
-		StandardRecordBlocker<VideoGame, Attribute> blocker = new StandardRecordBlocker<VideoGame, Attribute>(
-				new VideoGameBlockingKeyByTitleGenerator());
+		// StandardRecordBlocker<VideoGame, Attribute> blocker = new
+		// StandardRecordBlocker<VideoGame, Attribute>(
+		// new VideoGameBlockingKeyByTitleGenerator());
 		// sorted doesn't work? only 0 scores
-		// SortedNeighbourhoodBlocker<VideoGame, Attribute, Attribute> blocker = new
-		// SortedNeighbourhoodBlocker<>(
-		// new VideoGameBlockingKeyByPlatformGenerator(), 50);
-		// blocker.collectBlockSizeData("data/output/debugResultsBlocking.csv", 100);
+		SortedNeighbourhoodBlocker<VideoGame, Attribute, Attribute> blocker = new SortedNeighbourhoodBlocker<>(
+				new VideoGameBlockingKeyByTitleGenerator(), 75);
+		blocker.collectBlockSizeData("data/output/debugResultsBlocking.csv", 100);
 
 		// Initialize Matching Engine
 		MatchingEngine<VideoGame, Attribute> engine = new MatchingEngine<>();
@@ -104,7 +122,8 @@ public class IR_using_machine_learning_wikidata_sales_daniel {
 		// hier momentan noch selber goldstandard wie oben ???
 		System.out.println("*\n*\tLoading gold standard\n*");
 		MatchingGoldStandard gsTest = new MatchingGoldStandard();
-		gsTest.loadFromCSVFile(new File("data/goldstandard/gold-standard_wikidata_sales.csv"));
+		gsTest.loadFromCSVFile(
+				new File("data/goldstandard/wikidata_sales/gold-standard_wikidata_sales_test_66_33.csv"));
 
 		// evaluate your result
 		System.out.println("*\n*\tEvaluating result\n*");
