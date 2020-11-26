e03f5fb69965793dfed2fa67c71c9670e1284acf
lenaaaa <>
2020-11-26 08:37:09 +0100

    updated ML sales<->steam

---
+++ 2-identity-resolution/IdentityResolutionProject/src/main/java/de/uni_mannheim/informatik/web_data_integration/matching_rules/IR_using_machine_learning_sales_steam_lena.java
@@ -0,0 +1,134 @@
+package de.uni_mannheim.informatik.web_data_integration.matching_rules;
+
+import java.io.File;
+
+import de.uni_mannheim.informatik.web_data_integration.comparator.PlatformComparatorAdvanced;
+import de.uni_mannheim.informatik.web_data_integration.comparator.TitleComparator;
+import de.uni_mannheim.informatik.web_data_integration.comparator.custom_similarity_measure.JaroWinklerSimilarity;
+import org.slf4j.Logger;
+
+
+import de.uni_mannheim.informatik.dws.winter.matching.MatchingEngine;
+import de.uni_mannheim.informatik.dws.winter.matching.MatchingEvaluator;
+import de.uni_mannheim.informatik.dws.winter.matching.algorithms.RuleLearner;
+import de.uni_mannheim.informatik.dws.winter.matching.blockers.SortedNeighbourhoodBlocker;
+import de.uni_mannheim.informatik.dws.winter.matching.rules.WekaMatchingRule;
+import de.uni_mannheim.informatik.dws.winter.model.Correspondence;
+import de.uni_mannheim.informatik.dws.winter.model.HashedDataSet;
+import de.uni_mannheim.informatik.dws.winter.model.MatchingGoldStandard;
+import de.uni_mannheim.informatik.dws.winter.model.Performance;
+import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
+import de.uni_mannheim.informatik.dws.winter.model.io.CSVCorrespondenceFormatter;
+import de.uni_mannheim.informatik.dws.winter.processing.Processable;
+import de.uni_mannheim.informatik.dws.winter.similarity.string.JaccardOnNGramsSimilarity;
+import de.uni_mannheim.informatik.dws.winter.similarity.string.LevenshteinSimilarity;
+import de.uni_mannheim.informatik.dws.winter.similarity.string.TokenizingJaccardSimilarity;
+import de.uni_mannheim.informatik.dws.winter.utils.WinterLogManager;
+import de.uni_mannheim.informatik.web_data_integration.blocking.VideoGameBlockingKeyByTitleGenerator;
+<<<<<<< HEAD
+import de.uni_mannheim.informatik.web_data_integration.comparator.PlatformComparator;
+import de.uni_mannheim.informatik.web_data_integration.comparator.PubDateComparator;
+import de.uni_mannheim.informatik.web_data_integration.comparator.PublisherComparator;
+=======
+>>>>>>> d4bf4ae48e43f548897ba6aadb3147607bd28291
+import de.uni_mannheim.informatik.web_data_integration.model.VideoGame;
+import de.uni_mannheim.informatik.web_data_integration.model.VideoGameXMLReader;
+
+public class IR_using_machine_learning_sales_steam {
+
+private static final Logger logger = WinterLogManager.activateLogger("default");
+	
+    public static void main( String[] args ) throws Exception
+    {
+    	// loading data 
+		System.out.println("*\n*\tLoading datasets\n*");
+        HashedDataSet<VideoGame, Attribute> dataVideoGameSteam = new HashedDataSet<>();
+        new VideoGameXMLReader().loadFromXML(new File("data/input/steam_mapping_output.xml"),
+                         "/videogames/videogame", dataVideoGameSteam);
+        HashedDataSet<VideoGame, Attribute> dataVideoGameSales = new HashedDataSet<>();
+        new VideoGameXMLReader().loadFromXML(new File("data/input/sales_mapping_output.xml"),
+                         "/videogames/videogame", dataVideoGameSales);
+		
+		// load the training set
+		MatchingGoldStandard gsTraining = new MatchingGoldStandard();
+	    gsTraining.loadFromCSVFile(new File("data/goldstandard/sales_steam/gold-standard_sales_steam_training_80.csv"));
+
+
+		// create a matching rule
+		String options[] = new String[] { "-S" };
+		String modelType = "SimpleLogistic"; // use a logistic regression
+		WekaMatchingRule<VideoGame, Attribute> matchingRule = new WekaMatchingRule<>(0.7, modelType, options);
+		matchingRule.activateDebugReport("data/output/sales_steam/1debugResultsMatchingRule.csv", 1000, gsTraining);
+		
+		// add comparators
+<<<<<<< HEAD
+		 matchingRule.addComparator(new TitleComparator(new TokenizingJaccardSimilarity()));
+         matchingRule.addComparator(new PlatformComparator(new TokenizingJaccardSimilarity()));
+         matchingRule.addComparator(new PubDateComparator(10));
+         matchingRule.addComparator(new PublisherComparator(new JaroWinklerSimilarity()));
+=======
+		// Title
+  //	matchingRule.addComparator(new VideoGameTitleComparatorEqual());
+		matchingRule.addComparator(new TitleComparator(new LevenshteinSimilarity()));
+		matchingRule.addComparator(new TitleComparator(new TokenizingJaccardSimilarity()));
+//		
+		// Platform
+  	    matchingRule.addComparator(new PlatformComparatorAdvanced(new TokenizingJaccardSimilarity()));
+        matchingRule.addComparator(new PlatformComparatorAdvanced(new LevenshteinSimilarity()));
+//     
+		// Publisher
+//      matchingRule.addComparator(new VideoGamePublisherComparatorJaccard());
+//      matchingRule.addComparator(new VideoGamePublisherComparatorLevenshtein());
+//      matchingRule.addComparator(new VideoGamePublisherComparatorEqual());
+>>>>>>> d4bf4ae48e43f548897ba6aadb3147607bd28291
+
+
+		
+		// train the matching rule's model
+		System.out.println("*\n*\tLearning matching rule\n*");
+		RuleLearner<VideoGame, Attribute> learner = new RuleLearner<>();
+		learner.learnMatchingRule(dataVideoGameSteam, dataVideoGameSales, null, matchingRule, gsTraining);
+		System.out.println(String.format("Matching rule is:\n%s", matchingRule.getModelDescription()));
+		
+		// create a blocker (blocking strategy)
+        SortedNeighbourhoodBlocker<VideoGame, Attribute, Attribute> blocker = new SortedNeighbourhoodBlocker<>(new VideoGameBlockingKeyByTitleGenerator(), 20);
+
+        blocker.collectBlockSizeData("data/output/sales_steam/debugResultsBlocking.csv", 100);
+		
+		// Initialize Matching Engine
+		MatchingEngine<VideoGame, Attribute> engine = new MatchingEngine<>();
+
+		// Execute the matching
+		System.out.println("*\n*\tRunning identity resolution\n*");
+		Processable<Correspondence<VideoGame, Attribute>> correspondences = engine.runIdentityResolution(
+				dataVideoGameSteam, dataVideoGameSales, null, matchingRule,
+				blocker);
+
+		// write the correspondences to the output file
+		new CSVCorrespondenceFormatter().writeCSV(new File("data/output/sales_steam/sales_steam_correspondences.csv"), correspondences);	
+		
+		
+	
+		// load the gold standard (test set)
+        System.out.println("*\n*\tLoading gold standard\n*");
+ 		MatchingGoldStandard gsTest = new MatchingGoldStandard();
+ 		gsTest.loadFromCSVFile(new File(
+            "data/goldstandard/sales_steam/gold-standard_sales_steam_test_20.csv"));
+ 		
+		
+		// evaluate your result
+		System.out.println("*\n*\tEvaluating result\n*");
+		MatchingEvaluator<VideoGame, Attribute> evaluator = new MatchingEvaluator<VideoGame, Attribute>();
+		Performance perfTest = evaluator.evaluateMatching(correspondences,
+				gsTest);
+		
+		// print the evaluation result
+		System.out.println("Steam <-> Sales");
+		System.out.println(String.format(
+				"Precision: %.4f",perfTest.getPrecision()));
+		System.out.println(String.format(
+				"Recall: %.4f",	perfTest.getRecall()));
+		System.out.println(String.format(
+				"F1: %.4f",perfTest.getF1()));
+    }
+}
